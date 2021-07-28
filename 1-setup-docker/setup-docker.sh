2172738e1e757605c35268b317689de29edc9574
Focus <hieunt9@gmail.com>
2021-07-28 19:57:43 +0700

    update 28Jul2021

--- 1-setup-docker/setup-docker.sh
+++ 1-setup-docker/setup-docker.sh
@@ -61,6 +61,12 @@ systemctl daemon-reload
 systemctl restart docker
 systemctl enable docker
 
+# Install Docker Compose
+
+sudo curl -Ls "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+
 #########################################################################################
 # SECTION 3: FINISHED
 
