c41cf230be2162274e1d0e23158c84917bec29e6
Focus <hieunt9@gmail.com>
2021-08-29 16:22:46 +0700

    update 29Aug2021:16:22

---
+++ 2-deploy-portainer/bootstrap.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+# Script deploy portainer with docker
+
+##########################################################################################
+# SECTION 1: PREPARE
+
+# change root
+sudo -i
+sleep 2
+
+# update system
+# yum clean all
+# yum -y update
+# sleep 1
+
+# config timezone
+timedatectl set-timezone Asia/Ho_Chi_Minh
+
+# disable SELINUX
+setenforce 0 
+sed -i 's/enforcing/disabled/g' /etc/selinux/config
+
+# disable firewall
+systemctl stop firewalld
+systemctl disable firewalld
+
+# config hostname
+hostnamectl set-hostname docker1
+
+# config file host
+cat >> "/etc/hosts" <<END
+ 172.20.10.10 docker1 docker1.hit.local
+END
+
+##########################################################################################
+# SECTION 2: INSTALL Docker, Docker-compse, Portainer, git
+
+# Install docker
+curl -fsSL https://get.docker.com/ | sh
+systemctl start docker
+systemctl enable docker
+
+# Install docker-compose
+sudo curl -sL "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+
+# install git
+yum -y install git
+
+#########################################################################################
+# SECTION 3: DEPLOY portainer
+
+# clone repo from github
+cd ~
+git clone https://github.com/hieunt84/play-docker.git
+
+# change working directory
+cd ./play-docker/2-deploy-portainer/ 
+
+# deploy portainer
+docker-compose up -d
+
+# verify
+docker-compose ps
+
+#########################################################################################
+# SECTION 4: FINISHED
+
+# config firwall
+systemctl start firewalld
+systemctl enable firewalld
+sudo firewall-cmd --zone=public --permanent --add-port=8069/tcp
+# Open Port for link Portainer
+sudo firewall-cmd --zone=public --permanent --add-port=2375/tcp
+sudo firewall-cmd --reload
+sudo systemctl restart firewalld
+
+# notification
+echo " DEPLOY COMPLETELY"
